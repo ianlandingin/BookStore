835adad57dcbc81570ea4ad38ca8e08e942c5775
ianlandingin <iclandingin28@gmail.com>
2023-11-12 22:27:47 +0800

    used bookmodel.js to create a book model and created a post endpoint to create a new book, used postman/thunder client to test and added middleware for express json

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,56 @@
 import express from "express";
-import { PORT,mongoDBURL } from "./config.js";
-import mongoose from 'mongoose';
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
 
 const app = express();
 
-app.get('/', (request, response) => {
+// Middleware for parsing body
+app.use(express.json());
+
+app.get("/", (request, response) => {
   console.log(request);
-  return response.status(234).send("Welcome to your first API endpoint in nodejs")
+  return response
+    .status(234)
+    .send("Welcome to your first API endpoint in nodejs");
+});
+
+// Route for Save a new Book
+
+app.post("/books", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "Title, Author, and Publish year fields are required.",
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publishYear: request.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return response.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send(error.message);
+  }
 });
 
 mongoose
-.connect(mongoDBURL)
-.then(() => {
-  console.log('App is connected to database');
-  app.listen(PORT, () => {
-    console.log(`App is listening to port: ${PORT}`);
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App is connected to database");
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
   });
-})
-.catch((e) => {
-console.log(e);
-});
