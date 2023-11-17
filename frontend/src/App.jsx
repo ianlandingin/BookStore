ba0fe310b1999d521ac0f6f016eb85e4b3f70676
ianlandingin <iclandingin28@gmail.com>
2023-11-17 12:45:08 +0800

    Created pages for delete,edit,create,show, and home. used Router in main.jsx for SPA, and added routes in app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,24 @@
 // eslint-disable-next-line no-unused-vars
-import { React, useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+// import CreateBook from "./pages/CreateBook.jsx";
+// import DeleteBook from "./pages/DeleteBook.jsx";
+// import EditBook from "./pages/EditBook.jsx";
+// import ShowBook from "./pages/ShowBook.jsx";
 // import axios from "axios";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="" element={""} />
-      <Route path="" element={""} />
-      <Route path="" element={""} />
-      <Route path="" element={""} />
-      <Route path="" element={""} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/books/create" element={<CreateBook />} />
+        <Route path="/books/details/:id" element={<ShowBook />} />
+        <Route path="/books/edit/:id" element={<EditBook />} />
+        <Route path="/books/delete/:id" element={<DeleteBook />} /> */}
+      </Routes>
+    </>
   );
 };
 
