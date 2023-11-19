68b7ec607f11fef96aad5bddcfbbb91dc490d44a
ianlandingin <iclandingin28@gmail.com>
2023-11-19 17:56:43 +0800

    Prep Edit book function component page

---
+++ frontend/src/pages/EditBook.jsx
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const EditBook = () => {
+  return (
+    <h1>You are in the Edit book</h1>
+  )
+}
+
+export default EditBook
