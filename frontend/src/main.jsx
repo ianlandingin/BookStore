8fcbedbdacc709d187b10bf33d7614b2aab2f76d
ianlandingin <iclandingin28@gmail.com>
2023-11-20 22:46:08 +0800

    Initialized notistack in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
-    <App />
+    <SnackbarProvider>
+      <App />
+    </SnackbarProvider>
   </Router>
 );
